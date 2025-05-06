ee38b9c47274c055a25e2793986655feaafedfbd
coder-pink <coderpink17@gmail.com>
2025-05-06 15:50:07 +0530

    added booking page

---
+++ src/components/MultiStepForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MultiStepForm() {
+  return (
+    <div>
+      Form
+    </div>
+  )
+}
+
+export default MultiStepForm
---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,14 @@
+import MultiStepForm from '../components/MultiStepForm';
+
+export default function BookingPage() {
+  return (
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center text-gray-800 mb-8">
+          Vehicle Booking Form
+        </h1>
+        <MultiStepForm />
+      </div>
+    </div>
+  );
+}
